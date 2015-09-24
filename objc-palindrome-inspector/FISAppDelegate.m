01d83b84abfc3eb789201fce5c6102b1828c7ccb
giwankim <giwan.kim@gmail.com>
2015-09-24 15:14:34 -0400

    More?

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -15,4 +15,32 @@
  
  */
 
+-(BOOL)stringIsPalindrome:(NSString *)string {
+    NSArray *punctuations = @[ @",", @".", @"!", @";", @"?"];
+    
+    NSString *lowercase = [string lowercaseString];
+    
+    NSString *spaceless = [lowercase stringByReplacingOccurrencesOfString:@" "
+                                                               withString:@""];
+    NSString *withoutPunctuations = [spaceless copy];
+    for (NSString *punctuation in punctuations) {
+        withoutPunctuations = [withoutPunctuations stringByReplacingOccurrencesOfString:punctuation
+                                                                             withString:@""];
+    }
+    
+    NSString *reverseString = [self stringByReversingString:withoutPunctuations];
+    BOOL result = [reverseString isEqualToString:withoutPunctuations];
+    return result;
+}
+
+-(NSString *)stringByReversingString:(NSString *)string {
+    NSString *result = @"";
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    return result;
+}
+
 @end
