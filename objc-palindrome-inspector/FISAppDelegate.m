c7164d5c2de5e2004199acbce35a3da632355dc2
asbhat <aditya.s.bhat@gmail.com>
2015-11-27 10:23:20 -0500

    palindrome detector with tests

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -9,10 +9,34 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    NSArray *charactersToBeStripped = @[@".", @",", @"!", @"?", @" "];
+    NSString *stripped = [string copy];
+    
+    for (NSUInteger i = 0; i < [charactersToBeStripped count]; i++) {
+        stripped = [stripped stringByReplacingOccurrencesOfString:charactersToBeStripped[i] withString:@""];
+    }
+    
+    NSString *lowercase = [stripped lowercaseString];
+    NSString *reversed = [self stringByReversingString:lowercase];
+    
+    BOOL isPalindrome = [reversed isEqualToString:lowercase];
+    
+    return isPalindrome;
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    
+    return result;
+}
 
 @end
