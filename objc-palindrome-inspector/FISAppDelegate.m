195d489312e9f3292e36d0d4fd4e3f35ba6f3c41
lianaluci <llgrobstein@gmail.com>
2015-11-30 23:17:01 -0500

    pass racecar test

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -9,10 +9,27 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    NSString *reverse = [self stringByReversingString:string];
+    BOOL stringIsEqualToReverse = [string isEqualToString:reverse];
+    
+    return stringIsEqualToReverse;
+}
 
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%C", c];
+        
+    }
+    
+    return result;
+    
+
+}
 @end
