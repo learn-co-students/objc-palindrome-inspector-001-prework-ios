eda952be0cda5afa164f1ffde2922aedac097f4d
AliMar11 <alicia.marisal@gmail.com>
2015-12-07 01:33:00 -0500

    Palindrome Inspector Complete^_^

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -9,10 +9,35 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    NSArray  *englishFun = @[ @".", @"!", @",", @"?", @";", @":"];
+    NSString *deletingTheFun = [string copy];
+    for (NSUInteger i = 0; i < [englishFun count]; i++) {
+        NSString *delEngFun = englishFun[i];
+        deletingTheFun = [deletingTheFun stringByReplacingOccurrencesOfString:delEngFun withString:@""];
+    }
+    NSString *spaceDelete = [deletingTheFun stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSString *lowerCase = [spaceDelete lowercaseString];
+    NSString *reverse = [self stringByReversingString:lowerCase];
+   
+    
+    BOOL stringIsEqualToReverse = [lowerCase isEqualToString:reverse];
+    
+    return stringIsEqualToReverse;
+}
+
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    
+    return result;
+}
+
 
 @end
