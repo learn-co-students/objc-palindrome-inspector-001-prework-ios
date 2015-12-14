87e025002821cf8072d3af1aeb29cc11dd7b5177
FedericoPaliotta <federico.paliotta@gmail.com>
2015-12-14 20:17:12 +0100

    Done.

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -9,10 +9,29 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    NSString * originalWellFormattedString = [[string lowercaseString] stringByReplacingOccurrencesOfString:@" " withString:@""];
+    
+    NSArray *punctuations = @[ @".", @",", @"!", @"?", @":", @";" ];
+    
+    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+        NSString *punctuation = punctuations[i];
+        originalWellFormattedString = [originalWellFormattedString stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
 
+    NSString * reversedString = [self stringByReversingString:originalWellFormattedString];
+    return [originalWellFormattedString isEqualToString:reversedString];
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    
+    return result;
+}
 @end
