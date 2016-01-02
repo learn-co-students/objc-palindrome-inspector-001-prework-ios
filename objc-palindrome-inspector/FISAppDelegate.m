7f7b5b0fc0e2e105de8168d7275564929f8b95b6
fleish007 <cfleish@gmail.com>
2016-01-02 18:11:31 -0500

    palindrome inspector perfector

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -15,4 +15,38 @@
  
  */
 
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    NSString *lowercase = [string lowercaseString];
+    
+    NSArray *punctuations = @[@",", @".", @"!", @";", @":", @"?"];
+    NSString *noPunctuation = [lowercase copy];
+    
+    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+        NSString *punctuationString = punctuations[i];
+        
+        noPunctuation = [noPunctuation stringByReplacingOccurrencesOfString:punctuationString withString:@""];
+    }
+    
+    NSString *noWhiteSpace = [noPunctuation stringByReplacingOccurrencesOfString:@" " withString:@""];
+    
+    NSString *reverse = [self stringByReversingString:noWhiteSpace];
+    BOOL isReverse = [noWhiteSpace isEqualToString:reverse];
+    
+    return isReverse;
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    
+    return result;
+}
+
 @end
