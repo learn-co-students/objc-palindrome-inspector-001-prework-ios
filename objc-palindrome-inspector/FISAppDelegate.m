3de9dc2e4d2c8ae1f448d7dcd6f56c5055abaa22
kevinkong91 <kevinkong91@gmail.com>
2016-01-17 18:05:44 -0500

    palindrome inspector - TDD Expecta

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -6,13 +6,49 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    
+    // Space
+    NSString *sanitizedString = [string stringByReplacingOccurrencesOfString:@" " withString:@""];
+    
+    // Punctuation
+    NSArray *punctuations = @[@".",@",",@"!",@"?",@"-",@"_",@"/",@"|"];
+    
+    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+        sanitizedString = [sanitizedString stringByReplacingOccurrencesOfString:punctuations[i] withString:@""];
+    }
+    
+    
+    // Caps
+    sanitizedString = [sanitizedString lowercaseString];
+    
+    NSString *reversed = [self stringByReversingString:sanitizedString];
+    
+    BOOL isPalindrome = [sanitizedString isEqualToString:reversed];
+    
+    return isPalindrome;
+    
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    
+    return result;
+
+}
 
 @end
