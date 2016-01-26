73aad39036f7c9ade5126a4f5cbdcf49b58a3686
colinfwalsh <colinfwalsh@gmail.com>
2016-01-26 18:59:06 -0500

    Palindrome Inspector

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -6,6 +6,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSString *racecar = @"racecar";
+    
+    
+    
+    NSString *bob = @"Bob";
+    NSString *kanakanak = @"Kanakanak";
+    NSString *aibohphobia = @"Aibohphobia";
+    
+    BOOL racecareIsPalindrome = [self stringIsPalindrome:racecar];
+    BOOL bobIsPalindrome = [self stringIsPalindrome:bob];
+    BOOL kanakanakIsPalindrome = [self stringIsPalindrome:kanakanak];
+    BOOL aibophobiaIsPalindrome = [self stringIsPalindrome:aibohphobia];
     return YES;
 }
 
@@ -14,5 +26,35 @@
  * Implement your methods here.
  
  */
+
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    NSArray *punctuations = @[@".", @",", @"!", @"?", @":", @";"];
+    NSString *withoutPunctuation = [string copy];
+    
+    for (NSUInteger i = 0; i < [punctuations count]; i++){
+        NSString *punctuation = punctuations[i];
+        withoutPunctuation = [withoutPunctuation stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    
+    NSString *spaceless = [withoutPunctuation stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSString *lowercase = [spaceless lowercaseString];
+    NSString *reverse = [self stringByReversingString:lowercase];
+    
+    BOOL stringIsEqualToReverse = [lowercase isEqualToString:reverse];
+    
+    return stringIsEqualToReverse;
+    }
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    
+    return result;
+}
 
 @end
