43817e9c51e6e00607ac1281dec3407930808fd4
hdinhof1 <hdinhofer@gmail.com>
2016-03-31 11:38:53 -0400

    Done.

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -6,13 +6,41 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
     return YES;
 }
-
 /**
  
  * Implement your methods here.
  
  */
+
+-(BOOL) stringIsPalindrome:(NSString *)string {
+    
+    NSString *lowercase = [string lowercaseString];
+    NSArray *excessChar = @[@"!", @"/", @".", @"-", @" ", @","];
+    for (NSUInteger i = 0; i<excessChar.count; i++) {
+       lowercase = [lowercase stringByReplacingOccurrencesOfString:excessChar[i] withString:@""];
+    }
+    NSString *reverse = [self stringByReversingString:lowercase];
+    BOOL isPalindrome = [reverse isEqualToString:lowercase];
+    
+    
+    
+    return isPalindrome;
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    
+    return result;
+}
 
 @end
