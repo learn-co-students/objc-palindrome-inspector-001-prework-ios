ec9005b18f9b1349e939f3f9c28b40b3c4079abf
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-04-07 14:57:36 -0400

    Palindrome Inspector complete

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -4,6 +4,48 @@
 
 @implementation FISAppDelegate
 
+
+- (BOOL)stringIsPalindrome:(NSString *)string {
+   
+  
+    
+    NSArray *punctuations = @[@".", @",", @"!", @"?", @":", @";"];
+    NSString *withoutPunctuation = [string copy];
+    
+    for (NSUInteger i = 0; i < [punctuations count]; i++ ) {
+        NSString *punctuation = punctuations[i];
+        withoutPunctuation = [withoutPunctuation stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    
+    NSString *spaceless = [withoutPunctuation stringByReplacingOccurrencesOfString:@" " withString:@""];
+    //can handle palindromes with spaces
+    
+    NSString *lowercase = [spaceless lowercaseString];
+    // definining string to be lowercase, can handle uppercase palindromes
+    
+    NSString *reverse = [self stringByReversingString:lowercase];
+   // defining reverse
+    
+    BOOL stringIsEqualToReverse = [lowercase isEqualToString:reverse];
+    
+    return stringIsEqualToReverse;
+}
+
+
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    return result;
+    }
+
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;
