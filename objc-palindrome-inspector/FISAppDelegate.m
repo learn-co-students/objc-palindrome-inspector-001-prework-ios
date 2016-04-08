9dc7800b8b6ff5f2374c98896b976d2a7fb47643
github username <github email address>
2016-04-07 20:20:14 -0400

    lab complete

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -9,10 +9,34 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+
+-(BOOL)stringIsPalindrome:(NSString *)string {
+    
+    NSString *lowercaseString = [string lowercaseString];
+    NSArray *punctuation = @[@" ", @",", @".", @";", @"!", @"?", @":"];
+    NSString *stringWithoutPunctuation = [lowercaseString copy];
+    
+    for (NSUInteger i =0; i < [punctuation count]; i++) {
+        stringWithoutPunctuation = [stringWithoutPunctuation stringByReplacingOccurrencesOfString:punctuation[i] withString:@""];
+    }
+    
+    NSString *reversedString = [self stringByReversingString:stringWithoutPunctuation];
+    
+    if ([stringWithoutPunctuation isEqualToString:reversedString]) {
+        return YES;
+    }
+    return NO;
+}
+
+-(NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        result = [result stringByAppendingFormat:@"%C",[string characterAtIndex:index]];
+    }
+    
+    return result;
+}
 
 @end
