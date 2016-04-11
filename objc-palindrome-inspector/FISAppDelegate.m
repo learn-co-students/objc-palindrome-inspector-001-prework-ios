6c309a32cda245d24956d0db88e1b0d0a34574f2
dochonglo <ismaelbarry777@hotmail.com>
2016-04-11 19:28:07 -0400

    Lab 9: Unit Testing for Palindrome Inspector is complete.

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -15,4 +15,32 @@
  
  */
 
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    NSArray *punctuations = @[@",", @";", @":", @"!", @"."];
+    NSString *noPunctuation = [string copy];
+    for (NSUInteger i=0; i<[punctuations count]; i++) {
+        NSString *punctuation = punctuations[i];
+        noPunctuation = [noPunctuation stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    
+    NSString *noSpace = [noPunctuation stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSString *lowercase = [noSpace lowercaseString];
+    NSString *stringReversal = [self stringByReversingString:lowercase];
+    BOOL stringIsEqualToReverse = [lowercase isEqualToString:stringReversal];
+    return stringIsEqualToReverse;
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    NSString *result = @"";
+    for (NSUInteger i = [string length]; i>0; i--) {
+        NSUInteger index = i-1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%C", c];
+    }
+    
+    return result;
+}
+
+
 @end
