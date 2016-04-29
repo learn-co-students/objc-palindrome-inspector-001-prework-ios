07cf2f82e3fbb4e8dd918797562f649b12c13dd2
bsoung <bsoung@gm.slc.edu>
2016-04-29 12:35:31 -0400

    first commit

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -9,10 +9,32 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (BOOL)stringIsPalindrome: (NSString *)string {
+    
+    NSArray *punctuations = @[@",", @".", @":", @";", @"!", @"?"];
+    for (NSString *punctuation in punctuations) {
+        string = [string stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    string = [string stringByReplacingOccurrencesOfString:@" " withString:@""];
+    string = [string lowercaseString];
+    NSString *reverseString = [self stringByReversingString:string];
+    BOOL isPalindrome = [string isEqualToString:reverseString];
+    
+    return isPalindrome;
+}
+
+- (NSString *)stringByReversingString: (NSString *)string {
+    
+    NSMutableString *reverseString = [NSMutableString string];
+    NSUInteger index = [string length];
+    
+    while (index > 0) {
+        index--;
+        NSRange subStringRange = NSMakeRange(index, 1);
+        [reverseString appendString:[string substringWithRange:subStringRange]];
+    }
+    
+    return reverseString;
+}
 
 @end
