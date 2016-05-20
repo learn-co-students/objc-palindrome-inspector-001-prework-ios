8d763369a83116e78abedd124e445536002c128f
Salmaan Rizvi <sar228@cornell.edu>
2016-05-20 00:21:21 -0400

    palindrome instpector passed

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -9,10 +9,29 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    NSArray *punctuations = @[@".", @",", @";", @":", @"!", @" "];
+    NSString *lowercase = [string lowercaseString];
+    NSString *withoutPunctOrSpace = lowercase;
+    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+        withoutPunctOrSpace = [withoutPunctOrSpace stringByReplacingOccurrencesOfString:punctuations[i] withString:@""];
+    }
+    NSString *reverse = [self stringByReversingString:withoutPunctOrSpace];
+    return [withoutPunctOrSpace isEqualToString:reverse];
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    
+    return result;
+}
 
 @end
