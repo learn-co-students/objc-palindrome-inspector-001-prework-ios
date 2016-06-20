947b0f5a5ee9bbbc29c1d569058c07289d214b68
henrykchan <chan.henryk@gmail.com>
2016-06-20 16:13:29 -0400

    palindrome-inspector

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -15,4 +15,36 @@
  
  */
 
+-(BOOL)stringIsPalindrome:(NSString *)string{
+    
+    NSArray *punctuations = @[ @".", @",", @"!", @"?", @":", @";"];
+    
+    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+        NSString *punctuation = punctuations[i];
+        string = [string stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    
+    NSString *spaceless = [ string stringByReplacingOccurrencesOfString:@" " withString:@""];
+    
+    NSString *uppercase = [ spaceless uppercaseString];
+    
+    NSString *reverse = [self stringByReversingString:uppercase];
+    
+    BOOL stringIsEqualToReverse = [ uppercase isEqualToString:reverse];
+    
+    return stringIsEqualToReverse;
+    }
+
+-(NSString *)stringByReversingString:(NSString *)string{
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [ string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%C", c];
+    }
+    return result;
+}
+
 @end
