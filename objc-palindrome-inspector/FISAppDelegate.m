db077d484be46eda11a5ed2779b44637990a8900
William Robinson <wsrobins@uvm.edu>
2016-06-21 16:44:55 -0400

    Palindrome testing // lab complete.

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -9,10 +9,41 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *reverse = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger charIndexInString = i - 1;
+        unichar c = [string characterAtIndex:charIndexInString];
+        reverse = [reverse stringByAppendingFormat:@"%c", c];
+    }
+    
+    return reverse;
+}
+
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    NSString *lowercase = [string lowercaseString];
+    NSString *withoutSpacesOrPunctuation = [lowercase copy];
+    NSString *reverse = @"";
+    NSArray *spaceAndPunctuations = @[@" ", @".", @",", @":", @";", @"?", @"!"];
+    BOOL isPalindrome = NO;
+    
+    for (NSUInteger i = 0; i < [spaceAndPunctuations count]; i++) {
+        
+        NSString *spaceOrPunctuation = spaceAndPunctuations[i];
+        withoutSpacesOrPunctuation = [withoutSpacesOrPunctuation stringByReplacingOccurrencesOfString:spaceOrPunctuation withString:@""];
+    }
+    
+    reverse = [self stringByReversingString:withoutSpacesOrPunctuation];
+    
+    if ([reverse isEqualToString:withoutSpacesOrPunctuation]) {
+        
+        isPalindrome = YES;
+    }
+    
+    return isPalindrome;
+}
 
 @end
