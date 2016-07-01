05cccb9152194135c5440a221b83c2b47f325589
Felicity <felicity.johnson.mail@gmail.com>
2016-07-01 18:06:36 -0400

    palindrome test

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -6,13 +6,47 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+    
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+
+- (BOOL) stringIsPalindrome:(NSString *)string {
+    
+    NSArray *punctuations = @[@".",@"!",@",",@"?",@":",@";"];
+    NSString *withoutPunctuations = [string copy];
+    
+    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+        
+        NSString *punctuation = punctuations[i];
+        withoutPunctuations = [withoutPunctuations stringByReplacingOccurrencesOfString:punctuation withString:@""];
+        
+    }
+    
+    NSString *spaceless = [withoutPunctuations stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSString *lowerCase = [spaceless lowercaseString];
+    NSString *reverse = [self stringByReversingString:lowerCase];
+    
+    
+    BOOL stringIsEqualToReverse = [lowerCase isEqualToString:reverse];
+    
+    return stringIsEqualToReverse;
+    
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%C", c];
+        
+    }
+    
+    return result;
+}
 
 @end
