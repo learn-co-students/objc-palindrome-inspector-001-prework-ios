da50811b22935967fdb3026017da429e9e0387e5
Forrest Zhao <fzhao@prosper.com>
2016-07-01 13:46:52 -0700

    complete

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -15,4 +15,30 @@
  
  */
 
+- (BOOL) stringIsPalindrome:(NSString *)string {
+    NSArray *punctuations = @[ @".", @",", @"!", @"?", @":", @";" ];
+    NSString *stringNoPunctuation = [string copy];
+    
+    for (NSString *punctuation in punctuations) {
+        stringNoPunctuation = [stringNoPunctuation stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    
+    NSString *stringAllLowerCase = [stringNoPunctuation lowercaseString];
+    NSString *stringNoSpaces = [stringAllLowerCase stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSString *reversedString = [self stringByReversingString:stringNoSpaces];
+    return [reversedString isEqualToString:stringNoSpaces];
+}
+
+- (NSString *) stringByReversingString:(NSString *)string {
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    
+    return result;
+}
+
 @end
